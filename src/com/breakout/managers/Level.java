d8001522a2310961785bf5d5d94d0d4c06d11da8
maine1506 <24021599@gmail.com>
2025-10-15 00:32:51 +0700

    feat(level): add Level class to load and generate brick layouts

---
+++ src/com/breakout/managers/Level.java
@@ -0,0 +1,42 @@
+package com.breakout.managers;
+
+import com.breakout.entities.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Level extends LevelData {
+    public static List<Brick> loadLevel(int id) {
+        switch (id) {
+            case 1: return createLevel(LevelData.level1);
+            case 2: return createLevel(LevelData.level2);
+            case 3: return createLevel(LevelData.level3);
+            default: return new ArrayList<>();
+        }
+    }
+
+    /**
+     * Return a map of bricks including normal bricks and explosive bricks.
+     * @param layout the layout of bricks
+     */
+    private static List<Brick> createLevel(int[][] layout) {
+        List<Brick> bricks = new ArrayList<>();
+
+        for (int row = 0; row < layout.length; row++) {
+            for (int col = 0; col < layout[row].length; col++) {
+                int type = layout[row][col];
+                double x = col * Brick.WIDTH;
+                double y = 50 + row * Brick.HEIGHT;
+
+                Brick b = null;
+                if (type == 1) {
+                    b = new Brick(x, y);
+                }
+                else if (type == 2) {
+                    b = new ExplosiveBrick(x, y, bricks, 1);
+                }
+                if (b != null) bricks.add(b);
+            }
+        }
+        return bricks;
+    }
+}
