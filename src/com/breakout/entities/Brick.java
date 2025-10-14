1eb10ef53591d2a29d6bd857835db41bded74fa3
maine1506 <24021599@gmail.com>
2025-10-15 00:41:15 +0700

    refactor(brick): move width/height to static constants and simplify constructor

--- src/com/breakout/entities/Brick.java
+++ src/com/breakout/entities/Brick.java
@@ -1,31 +1,25 @@
 package com.breakout.entities;
 
-import java.awt.*;
 import com.breakout.core.GameObject;
+
+import javax.swing.*;
 
 /**
  * Brick - can be drawn and destroyed.
  */
 public class Brick extends GameObject {
     private boolean destroyed = false;
+    public static final int WIDTH = 80;
+    public static final int HEIGHT = 20;
     
-    public Brick(double x, double y, double width, double height) {
-        super(x, y, width, height);
+    public Brick(double x, double y) {
+        super(x, y, WIDTH, HEIGHT);
+        sprite = new ImageIcon("assets/brick.png");
     }
     
     @Override
     public void update(double deltaTime) {
         // Bricks do not move
-    }
-    
-    @Override
-    public void draw(Graphics2D g) {
-        if (!destroyed) {
-            g.setColor(Color.decode("#fecbc2"));
-            g.fillRect((int)x, (int)y, (int)width, (int)height);
-            g.setColor(Color.BLACK);
-            g.drawRect((int)x, (int)y, (int)width, (int)height);
-        }
     }
     
     public void hit() {
