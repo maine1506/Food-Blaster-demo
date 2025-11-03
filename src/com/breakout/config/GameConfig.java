73df7345c530f9297c4cd8e29dfef9ddbb50960f
Nguyễn Minh Ánh <24021380@vnu.edu.vn>
2025-11-03 10:20:12 +0700

    chore: fix assets directories

--- src/com/breakout/config/GameConfig.java
+++ src/com/breakout/config/GameConfig.java
@@ -14,31 +14,32 @@ public class GameConfig {
     // ----- BALL -----
     public static final int BALL_WIDTH = 15;
     public static final int BALL_HEIGHT = 15;
-    public static final ImageIcon BALL_IMAGE = new ImageIcon("resources/assets/ball.png");
+    public static final ImageIcon BALL_IMAGE = new ImageIcon("src/com/breakout/resources.assets/ball.png");
     public static final double BALL_SPEED = 200;
 
     // ----- PADDLE -----
     public static final int PADDLE_WIDTH = 100;
     public static final int PADDLE_HEIGHT = 15;
-    public static final ImageIcon PADDLE_IMAGE = new ImageIcon("resources/assets/paddle.png");
+    public static final double PADDLE_SPEED = 400;
+    public static final ImageIcon PADDLE_IMAGE = new ImageIcon("src/com/breakout/resources.assets/paddle.png");
 
     // ----- BRICK -----
     public static final int BRICK_WIDTH = 80;
     public static final int BRICK_HEIGHT = 20;
-    public static final ImageIcon NORMAL_BRICK_IMAGE = new ImageIcon("resources/assets/brick.png");
+    public static final ImageIcon NORMAL_BRICK_IMAGE = new ImageIcon("src/com/breakout/resources.assets/brick.png");
 
     public static final int EXPLOSION_RADIUS = 1;    //explosion radius by bricks
-    public static final ImageIcon EXPLOSIVE_BRICK_IMAGE = new ImageIcon("resources/assets/explosiveBrick.png");
+    public static final ImageIcon EXPLOSIVE_BRICK_IMAGE = new ImageIcon("src/com/breakout/resources.assets/explosiveBrick.png");
 
     public static final double GRAVITY = 980; // Bricks fall faster over time
-    public static final ImageIcon FALLING_BRICK_IMAGE =  new ImageIcon("resources/assets/rottenCake.png");
+    public static final ImageIcon FALLING_BRICK_IMAGE =  new ImageIcon("src/com/breakout/resources.assets/rottenCake.png");
 
     public static final double ITEM_FALLING_SPEED = 150;
-    public static final ImageIcon ITEM_BRICK_IMAGE = new ImageIcon("resources/assets/chocolateCake.png");
+    public static final ImageIcon ITEM_BRICK_IMAGE = new ImageIcon("src/com/breakout/resources.assets/chocolateCake.png");
 
     // ----- ITEM -----
     public static final int ITEM_WIDTH = 0;
     public static final int ITEM_HEIGHT = 0;
-    public static final ImageIcon ITEM_IMAGE = new ImageIcon("resources/assets/gameOverItem.png");
+    public static final ImageIcon ITEM_IMAGE = new ImageIcon("src/com/breakout/resources.assets/gameOverItem.png");
     // Add items ...
 }
